5b007dffae7fe09f982fcbd535c8eff48df9c376
wiliender <wiliendersilva@gmail.com>
2023-12-25 23:14:01 -0300

    feat: creates PessoaService called by the application whenever the user needs to authenticate

---
+++ src/main/java/com/experian/serasa/rest/api/services/PessoaService.java
@@ -0,0 +1,22 @@
+package com.experian.serasa.rest.api.services;
+
+import com.experian.serasa.rest.api.repositories.PessoaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService implements UserDetailsService {
+
+  private final PessoaRepository repository;
+
+  public PessoaService(PessoaRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return repository.findByNome(username);
+  }
+}
