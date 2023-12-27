39d480f5e6b15812f0a986c06465b265a9fdb841
wiliender <wiliendersilva@gmail.com>
2023-12-27 19:10:57 -0300

    feat: create findByLogin

--- src/main/java/com/experian/serasa/rest/api/repositories/PessoaRepository.java
+++ src/main/java/com/experian/serasa/rest/api/repositories/PessoaRepository.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, String> {
+
+  UserDetails findByLogin(String login);
+
   UserDetails findByNome(String nome);
 
   UserDetails findByIdade(int idade);
