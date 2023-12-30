d6f5e035f3b378db06b6e0907659a689fa7fdb9a
wiliender <wiliendersilva@gmail.com>
2023-12-30 16:31:09 -0300

    feat: configuration admin user already registered in the database business rule only admin can register new people

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO pessoas (
+    IDADE, ROLE, SCORE, BAIRRO, CEP, CIDADE, ESTADO, ID, LOCALIDADE, LOGIN, LOGRADOURO, NOME, PASSWORD, TELEFONE, UF
+) VALUES (
+    26, '0', 1000, 'Distrito Industrial Miguel Abdelnur', '13571385', 'São Carlos', 'SP', 'dc50921c-4d6c-4a9a-aa48-7417e304f540',
+    'valor_localidade', 'admin', 'Avenida Doutor Heitor José Reali', 'admin', '$2a$10$mHjmrC.RpDz2yaU99iiuMeft86wrAEOCIyK6ntwe/aVKsFloyHjvO', '19999231095', 'valor_uf'
+);
